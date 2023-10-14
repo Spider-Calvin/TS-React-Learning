c9bfc65908b1b9f84abc118f1d1456e8932d85cb
Calvin Das <calvindas7@gmail.com>
2023-10-14 22:16:06 +0530

    form submit in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,30 @@ function App() {
     weight:0
   })
 
+  const submitfrom = (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    console.log(user);
+    alert(user);
+  }
+
+
   return (
     <>
       <div>
-        <form action="">
-          <input type="number" value={user?.age} onChange={(e)=>setUser((prev)=>({
+        <form action="" onSubmit={submitfrom}>
+          <input type="name" value={user?.name} onChange={(e)=>setUser((prev)=>({
+                ...prev!,
+                name: String( e.target.value)
+              }))
+            }
+            />
+          <input placeholder='age' type="number" value={user?.age} onChange={(e)=>setUser((prev)=>({
               ...prev!,
               age: Number( e.target.value)
             }))
           }
           />
+          <button>Submit</button>
         </form>
       </div>
     </>

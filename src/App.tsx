0bff8d679791f4b67897830cbf1a75e9241e97b2
Calvin Das <calvindas7@gmail.com>
2023-10-14 22:05:16 +0530

    useState and generic in type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import Box from './components/Box'
 
 function App() {
 
+  const [value, setValue] = useState<string>('')
+
   return (
     <>
       <div>
         hello
-        <Box heading='Spider calvin' children={<>"Hello baby"</>}/>
+        <Box lable='Spider calvin' value={value} setter={setValue}/>
       </div>
     </>
   )
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,17 +1,13 @@
-import { ReactNode } from "react";
+import { Dispatch, SetStateAction } from 'react'
 
-type propstype = {
-  heading:string,
-  count ?:number,
-  children :ReactNode
-}
-
-const Box = ({heading, count, children }:propstype) => {
+type inputs = string | number
+const Box = <T extends inputs>({lable, value, setter }:{ lable: string, value:T, setter:Dispatch<SetStateAction<T>> }) => {
   return (
     <>
-      <div>{heading}</div>
-      {count && <h1>{count}</h1> }
-      {children}
+     <form action="">
+      <label htmlFor="">{lable}</label>
+      <input type="text" value={value} onChange={(e)=>setter(e.target.value as T)}/>
+     </form>
     </>
   )
 }

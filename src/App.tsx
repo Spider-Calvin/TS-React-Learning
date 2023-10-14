dfb44b28e2a2d2616b33fa05117ef18907f8006e
Calvin Das <calvindas7@gmail.com>
2023-10-14 22:11:35 +0530

    interface or object setState in type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import Box from './components/Box'
+
+interface person {
+  name: string,
+  age: number,
+  weight: number
+}
 
 function App() {
 
-  const [value, setValue] = useState<string>('')
+  const [user, setUser] = useState<person>({
+    name:'',
+    age:0,
+    weight:0
+  })
 
   return (
     <>
       <div>
-        hello
-        <Box lable='Spider calvin' value={value} setter={setValue}/>
+        <form action="">
+          <input type="number" value={user?.age} onChange={(e)=>setUser((prev)=>({
+              ...prev!,
+              age: Number( e.target.value)
+            }))
+          }
+          />
+        </form>
       </div>
     </>
   )
